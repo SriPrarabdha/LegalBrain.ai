367760c0b4d4f846436033bd5a83ea872b36a6be
Himanshu <himanshu_co21a4_27@dtu.ac.in>
2023-05-28 16:05:53 +0530

    Update PricingBox.tsx

--- src/components/Pricing/PricingBox.tsx
+++ src/components/Pricing/PricingBox.tsx
@@ -10,21 +10,22 @@ const PricingBox = (props: {
   return (
 
     <div className="w-full ">
+      
       <div
-        className="wow fadeInUp relative z-10 rounded-md bg-white px-6 py-10 shadow-signUp dark:bg-[#1D2144]"
+        className="wow fadeInUp relative z-10 rounded-md bg-white px-6 py-10 shadow-signUp "
         data-wow-delay=".1s"
       >
         <div className="flex items-center justify-between mb-2">
-          <h3 className="price mb-2 text-3xl font-bold text-black dark:text-white">
+          <h3 className="price mb-2 text-3xl font-bold text-black ">
             $<span className="amount">{price}</span>
             <span className="time text-body-color">/{duration}</span>
           </h3>
-          <h4 className="mb-2 text-xl font-bold text-dark dark:text-white">
+          <h4 className="mb-2 text-xl font-bold text-dark">
             {packageName}
           </h4>
         </div>
         <p className="mb-7 text-base text-body-color">{subtitle}</p>
-        <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
+        <div className="mb-8 border-b border-body-color border-opacity-10 pb-8">
           <button className="flex w-full items-center justify-center rounded-md bg-slate-700 p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
             Start Free Trial
           </button>
