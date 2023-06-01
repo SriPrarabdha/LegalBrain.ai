f1d644c5d40213ed7ebff78a89febd8a8c27829a
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-06-01 17:26:09 +0530

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -92,7 +92,7 @@ const Footer = () => {
 
             <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
-                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
+                <h2 className="mb-10 text-xl font-bold text-black">
                   Useful Links
                 </h2>
                 <ul>
