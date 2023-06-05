52d23220aa3cba63a9ae32232e87d53c2f6ce7f7
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2023-06-05 12:21:30 +0530

    Footer Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default async function Home() {
         <Landing />    
         </main>
         <Features/>
-        <AboutSectionOne />
-        <AboutSectionTwo />
+        {/* <AboutSectionOne />
+        <AboutSectionTwo /> */}
         <Pricing/>
         {/* <Features/> */}
         
